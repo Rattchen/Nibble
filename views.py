8b103b6f8a92c742bb98e584deb4cc973b89596e
Rättchen <57540790+Rattchen@users.noreply.github.com>
2025-02-10 07:28:25 +0100

    Create simple IndexView

--- views.py
+++ views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+class IndexView(TemplateView):
+    template_name = 'nibble/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['message'] = 'Hi, welcome to Nibble'
+        return context
