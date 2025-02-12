c8ce19482df383051d6d2dd391933d96bfb4d0bb
Rättchen <57540790+Rattchen@users.noreply.github.com>
2025-02-12 12:46:38 +0100

    Create a functional board view

--- urls.py
+++ urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import IndexView
+from .views import IndexView, BoardView,CardView, Profileiew
 
 urlpatterns = [
     path('', IndexView.as_view(), name='index'),
+    path('board/', IndexView.as_view(), name='board_nothing'),
+    path('board/<int:pk>', BoardView.as_view(), name='board'),
 ]
--- views.py
+++ views.py
@@ -1,4 +1,5 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView
+from .models import Board
 
 class IndexView(TemplateView):
     template_name = 'nibble/index.html'
@@ -7,3 +8,17 @@ class IndexView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['message'] = 'Hi, welcome to Nibble'
         return context
+
+class BoardView(DetailView):
+    model = Board
+    template_name = 'nibble/board.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        board = self.get_object()
+        context["columns"] = board.columns.all()
+        for column in context['columns']:
+            column.card_list = column.cards.all()
+        return context
+
+
